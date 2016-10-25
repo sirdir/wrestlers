0a267a61426c9b084b5ac973b0d69f6a39eea73e
SirDiR <kriakaziaba@gmail.com>
2016-10-25 08:17:59 +0300

    some new tests

--- confLogin.js
+++ confLogin.js
@@ -2,5 +2,5 @@ exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
     // baseUrl: 'http://streamtv.net.ua/base',
-    specs: ['loginJora.js']
+    specs: ['main.js']
 };
