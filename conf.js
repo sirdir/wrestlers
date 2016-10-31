e7f0f69782c9d73c81fdf694e5bbb4572e335d75
SirDiR <kriakaziaba@gmail.com>
2016-10-31 05:17:01 +0300

    romive localizaed selenium hub

--- conf.js
+++ conf.js
@@ -1,11 +1,10 @@
 exports.config = {
     framework: 'jasmine',
-    seleniumAddress: 'http://localhost:4445/wd/hub',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
     // baseUrl: 'http://streamtv.net.ua/base',
     specs: ['./specs/wrestlers.js', './specs/login.js','./specs/search.js'],
     capabilities : {
         browserName: 'chrome',
-        seleniumAddress: 'http://localhost:4444/wd/hub',
         shardTestFiles: true,
         maxInstances: 1,
         count: 1
