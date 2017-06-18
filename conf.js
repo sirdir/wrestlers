7ce6873a9fcdb76301f08248e7edf90239d6dea2
sirdir <phddir@gmail.com>
2017-06-18 09:05:38 +0300

    add normal reporter withoud paralelling bugs

--- conf.js
+++ conf.js
@@ -1,5 +1,10 @@
 var shell = require('shelljs');
-var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+
+var reporter = new HtmlScreenshotReporter({
+    dest: 'target/screenshots',
+    filename: 'my-report.html'
+});
 
 
 exports.config = {
@@ -7,25 +12,30 @@ exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
 // baseUrl: 'http://streamtv.net.ua/base',
     onPrepare: function() {
-        jasmine.getEnv().addReporter(
-            new Jasmine2HtmlReporter({
-                savePath: 'target/screenshots'
-            })
-        );
+        jasmine.getEnv().addReporter(reporter);
     },
     beforeLaunch: () => {//todo redone wait of grid started
         shell.exec('docker-compose up -d', {silent: true});
         shell.exec('docker-compose logs', {silent: true});
+        process.on('uncaughtException', function () {
+            reporter.jasmineDone();
+            reporter.afterLaunch();
+        });
+        return new Promise(function(resolve){
+            reporter.beforeLaunch(resolve);
+        });
     },
-    afterLaunch: () => {
+    afterLaunch: (exitCode) => {
         shell.exec('docker-compose stop', {silent: true});
+        return new Promise(function(resolve){
+            reporter.afterLaunch(resolve.bind(this, exitCode));
+        });
     },
     specs: ['./specs/wrestlers.js', './specs/login.js','./specs/search.js'],
     capabilities : {
         browserName: 'chrome',
         shardTestFiles: true,
         maxInstances: 3,
-        count: 1
     },
     params: {
         login: 'auto',
