0b9528bcbbe7d9b10fa968c71e99a6b6158f9e04
sirdir <phddir@gmail.com>
2017-06-18 06:21:47 +0300

    add reporter

--- conf.js
+++ conf.js
@@ -1,10 +1,18 @@
 var shell = require('shelljs');
+var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 
 
 exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
 // baseUrl: 'http://streamtv.net.ua/base',
+    onPrepare: function() {
+        jasmine.getEnv().addReporter(
+            new Jasmine2HtmlReporter({
+                savePath: 'target/screenshots'
+            })
+        );
+    },
     beforeLaunch: () => {//todo redone wait of grid started
         shell.exec('docker-compose up -d', {silent: true});
         shell.exec('docker-compose logs', {silent: true});
